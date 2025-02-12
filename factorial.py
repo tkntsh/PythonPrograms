6d030d21210d9054b09b64710e17d112f5a67abb
Ntokozo <ntokntshangase@gmail.com>
2025-02-12 13:09:35 +0200

    Factorial Python file

---
+++ factorial.py
@@ -0,0 +1,15 @@
+#Author: Ntshangase Ntokozo
+#St: 4123601
+#Description: program accepts input from the user then calculates the factorial of that number 
+
+print("Welcome. This program calculates the factorial of an integer you entered\n")
+
+def factorial(x):
+  if(x==1):
+    return 1
+  else:
+    return x*factorial(x-1)
+
+num = int(input("Enter an integer: "))
+solution = factorial(num)
+print(num, "! = ", solution)
