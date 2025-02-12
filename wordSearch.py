779885b438aa4f3edce530515ee392d26ce63692
Ntokozo <ntokntshangase@gmail.com>
2025-02-12 13:18:03 +0200

    Update wordSearch.py

--- wordSearch.py
+++ wordSearch.py
@@ -1,5 +1,3 @@
-#Author: Ntshangase Ntokozo
-#St: 4123601
 #Description: Program that accepts a word from the user to mirror it
 
 print("Welcome. This program accepts input of two words to check if these words are the same or have the same elements")
