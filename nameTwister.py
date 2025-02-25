6ddf3f474f78fec286ce74f8835be9594544c435
Ntokozo <ntokntshangase@gmail.com>
2025-02-25 23:55:43 +0200

    Create nameTwister.py

---
+++ nameTwister.py
@@ -0,0 +1,7 @@
+# name twisting python program
+full_name = input("Enter your full name: ")
+
+print("Uppercase:", full_name.upper())
+print("Lowercase:", full_name.lower())
+print("Title case:", full_name.title())
+print(f"Hey {full_name}, your name is {len(full_name)} characters long (including the space)!")
