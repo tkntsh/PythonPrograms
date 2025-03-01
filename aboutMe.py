0fc6c57a3680687f44cf234880d2df26fb2c6940
Ntokozo <ntokntshangase@gmail.com>
2025-03-01 21:39:09 +0200

    About Me python program
    Uses input() for string, float, and integer types, converts numbers to strings for concatenation.

---
+++ aboutMe.py
@@ -0,0 +1,6 @@
+# about_me.py
+name = input("What’s your name? ")
+height = float(input("How tall are you (in meters)? "))
+siblings = int(input("How many siblings do you have? "))
+pizza = input("Do you like pizza (yes/no)? ")
+print(name + " is " + str(height) + " meters tall, has " + str(siblings) + " sibling(s), and likes pizza: " + pizza + "!")
