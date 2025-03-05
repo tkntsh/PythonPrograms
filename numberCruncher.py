f63e93ebeb773812f305a7cacffbceafb2f4e328
Ntokozo <ntokntshangase@gmail.com>
2025-03-05 14:32:37 +0200

    Number Cruncher Python program

---
+++ numberCruncher.py
@@ -0,0 +1,10 @@
+# number_cruncher.py
+a = 10
+b = 3
+print(str(a) + " + " + str(b) + " = " + str(a + b))
+print(str(a) + " - " + str(b) + " = " + str(a - b))
+print(str(a) + " * " + str(b) + " = " + str(a * b))
+print(str(a) + " / " + str(b) + " = " + str(a / b))
+print(str(a) + " % " + str(b) + " = " + str(a % b))
+print(str(a) + " ** " + str(b) + " = " + str(a ** b))
+print("Is " + str(a) + " greater than " + str(b) + "? " + str(a > b))
