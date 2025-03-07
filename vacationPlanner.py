03c1ac3c3fdbaa8db08880c14631f2489fb659f3
Ntokozo <ntokntshangase@gmail.com>
2025-03-07 15:33:32 +0200

    Vacation planner program
    Promit user input, return output using input

---
+++ vacationPlanner.py
@@ -0,0 +1,5 @@
+# vacation_planner.py
+destination = input("Where’s your dream vacation spot?: ")
+days = input("How many days would you stay?: ")
+activity = input("What’s one activity you’d do?: ")
+print("A " + days + "-day trip to " + destination + " with " + activity + " sounds amazing!")
